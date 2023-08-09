8ca433aaae27d051c2ba362b8daa0daa53fb5cbd
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-09 12:48:34 +0530

    creating object using js

---
+++ script.js
@@ -0,0 +1,8 @@
+const employee ={
+    name: 'mansoor ali khan',
+    hobbies:['singing','dancing','acting'],
+    profession: 'actor'
+}
+console.log(employee.name);
+console.log(employee.hobbies);
+console.log(employee.profession);
